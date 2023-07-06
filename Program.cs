75a34e683123f30cb803031c1e089887f37df895
Depress0 <segersian95@gmail.com>
2023-07-07 01:30:59 +0200

    switched to more secure randomizer

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Security.Cryptography;
 string symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890&é()§è!çà{[$@#]}?/:";
 byte symbolslength = (byte)symbols.Length;
 Console.Write("How long do you want your password to be (255 max): ");
 try{
     byte pwlength = Convert.ToByte(Console.ReadLine());
-    Random random = new Random();
     string password = "";
     for (int i = 0;i<pwlength;i++){
-        password = password + symbols[random.Next(0,symbolslength)];
+        password = password + symbols[RandomNumberGenerator.GetInt32(0,symbolslength)];
     }
     Console.WriteLine($"Your password is: {password}");
 }
